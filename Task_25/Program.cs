4e5bb72a69b886d48b1649a33de7b18d9bde2154
Карэн Саркисян <gugo@MacBook-Pro-Karen.local>
2022-10-30 21:52:55 +0200

    addae Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+// Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль.
+
+bool isParsedA = int.TryParse(Console.ReadLine(), out int a);
+bool isParsedB = int.TryParse(Console.ReadLine(), out int b);
+if (!isParsedA || !isParsedB) 
+{
+    Console.WriteLine("Неверно введены данные");
+    return;
+}
+
+int Powed(int firstNumber, int secondNumber)
+{
+    int result = 1;
+    for (int i = 0; i < secondNumber; i++)
+    {
+        result *= firstNumber;
+    }
+    return result;
+}
+
+int result = Powed(a,b);
+Console.WriteLine(result);
