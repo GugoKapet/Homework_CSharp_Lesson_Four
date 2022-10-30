744f9d9473eb2d8c8f8b9437a70d9c376f583317
Карэн Саркисян <gugo@MacBook-Pro-Karen.local>
2022-10-30 21:53:56 +0200

    added Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Задание должно быть выполнено в методах.
+
+
+int[] EightNumbersMassiv()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int[] result = EightNumbersMassiv();
+PrintArr(result);
+
+void PrintArr(int[] array) 
+{
+    Console.Write("[");
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
